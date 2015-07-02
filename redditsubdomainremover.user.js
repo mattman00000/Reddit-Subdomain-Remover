4f21f64623edb5d4f096a58e458efc876a13d998
mattman00000 <mattman00000@gmail.com>
2015-07-02 01:15:38 -0700

    Fixed indexOf
    indexOf is now set correctly to avoid infinite reload loop

--- redditsubdomainremover.user.js
+++ redditsubdomainremover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Reddit Subdomain Remover
 // @namespace  http://mattman00000.com
-// @version    0.1.1
+// @version    0.1.2
 // @description  Automatically redirects away from any subdomain
 // @match      *://*.reddit.com/*
 // @copyright  2015+, mattman00000
@@ -11,6 +11,6 @@
 
 console.warn("Activating Reddit Subdomain Remover");
 
-if (document.location.href.indexOf("reddit.com")!=11) {
+if (document.location.href.indexOf("reddit.com")!=12) {
 document.location="https://www.reddit.com".concat(document.location.pathname)
 }
