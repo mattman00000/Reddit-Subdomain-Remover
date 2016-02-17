fc8f6a61bb8abd3b7e2fdfb4883bd93a1e75168d
mattman00000 <mattman00000@gmail.com>
2016-02-17 01:51:54 -0800

    2.0.0 what the hell it shoulda already not had @updateURL etc

--- redditsubdomainremover.user.js
+++ redditsubdomainremover.user.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name       Reddit Subdomain Remover
-// @namespace  http://mattman00000.com
-// @version    1.1.1
+// @namespace  https://github.com/mattman00000/Reddit-Subdomain-Remover
+// @version    2.0.0
 // @description  Automatically redirects away from any subdomain
 // @match      *://*.reddit.com/*
 // @copyright  2015+, mattman00000
-// @updateURL https://github.com/mattman00000/Reddit-Subdomain-Remover/raw/master/redditsubdomainremover.user.js
-// @downloadURL https://github.com/mattman00000/Reddit-Subdomain-Remover/raw/master/redditsubdomainremover.user.js
 // ==/UserScript==
 
 console.warn("Activating Reddit Subdomain Remover");
